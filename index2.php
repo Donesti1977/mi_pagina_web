47b70297069f8794e39ce361b9f85b7475235c77
Ricardo Jurado <donesti1977@hotmail.com>
2025-08-01 01:16:13 +0200

    inicio portfolio

---
+++ index2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="estilos/estilos.css"/>
+    <title>RICARDO JURADO'S PROJECTS</title>
+</head>
+<body>
+    <div class="cabecera">
+        <header>
+            <h1>RICARDO JURADO'S PROJECTS</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="curriculum/cv_ricardo.v12.pdf" target="_blank">Curriculum Vitae</a></li>
+                    <li><a href="#">My Projects</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div class="menu_lateral">
+        <ul>
+            <li>Project1</li>
+            <li>Project2</li>
+            <li>Project3</li>
+            <li>Project4</li>
+            <li>Project5</li>
+            <li>Project6</li>
+            <li>Project7</li>
+        </ul>
+    </div>
+</body>
+</html>
